1873458b75400aaa6a6e99747a60a3811fdb1da1
transfyr <support@transfyr.com>
2019-12-31 07:14:42 +0300

    Sentiment Analysis project

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -72,6 +72,9 @@ def predict_fn(input_data, model):
 
     data_X = None
     data_len = None
+    
+    words = review_to_words(input_data)
+    data_X,data_len = convert_and_pad(model.word_dict, words)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -88,5 +91,9 @@ def predict_fn(input_data, model):
     #       be a numpy array which contains a single integer which is either 1 or 0
 
     result = None
+    
+    with torch.no_grad():
+        out = model.forward(data)
+    result = np.round(out.numpy())
 
     return result
